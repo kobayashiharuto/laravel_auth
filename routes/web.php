4d0682b3927d4f03788f18c3b05583be41ebdded
kl <datesaver1234@gmail.com>
2022-02-23 19:51:23 +0900

    ログインしているときのみアクセスできる controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/test', [App\Http\Controllers\TestContentController::class, 'index']);
